2a067478fcdcefbcf75c910b44ffda9516cf66b3
Gabriel <gabriel-imac@iMac-de-Gabriel.local>
2022-05-29 20:42:46 -0400

    Add cors and enable rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GithubModule } from './github/github.module';
 
 @Module({
-  imports: [GithubModule, ConfigModule.forRoot({isGlobal: true})],
+  imports: [GithubModule, 
+    ConfigModule.forRoot({isGlobal: true}),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 200,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,13 @@ import { ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
-  await app.listen(3000);
+  app.enableCors();
   app.setGlobalPrefix('/api');
   app.useGlobalInterceptors(
     new ClassSerializerInterceptor(app.get(Reflector))
   );
+  await app.listen(3000);
+
 }
 
 bootstrap();
